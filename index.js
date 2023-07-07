b4e541896edda5ff8db6962291fad026dc30907a
deiabasile <111993989+deiabasile@users.noreply.github.com>
2023-07-07 19:32:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ if (randomNumber1 > randomNumber2) {
 } else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins ⭐️";
 } else {
-  document.querySelector("h1").innerHTML = "Drawn!";
+  document.querySelector("h1").innerHTML = "Draw!";
 }
